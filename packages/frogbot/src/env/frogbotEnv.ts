818d4740463ea10680029cb3760f83613b282af2
Colby Gilbert <colby@firmware.ai>
2026-08-22 16:31:44 -0700

    feat(frogbot): add base environment schema

---
+++ packages/frogbot/src/env/frogbotEnv.ts
@@ -0,0 +1,11 @@
+import { env } from './builders.js'
+
+export const frogbotEnv = Object.freeze({
+  databaseUrl: env.string().required(),
+  frogbotSecret: env.string().required(),
+  logLevel: env
+    .enum(['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'silent'])
+    .default('info'),
+  nodeEnv: env.enum(['development', 'production', 'test']).default('production'),
+  port: env.number().default(3000),
+})
